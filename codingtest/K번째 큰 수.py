b748a466ffb140329fb5eb66bbc847d37211d947
TechNing39 <leeyujineee@gmail.com>
2025-11-21 19:55:28 +0900

    251121 K번째 큰 수

---
+++ codingtest/K번째 큰 수.py
@@ -0,0 +1,15 @@
+N,K=map(int,input().split())
+result=[]
+num=list(map(int,input().split()))
+res=set()
+for i in range(N):
+    for j in range(i+1,N):
+        for m in range(j+1,N):
+            res.add(num[i]+num[j]+num[m])
+           
+result=list(res)
+result.append(result.sort(reverse=True))
+print(result)
+print(result[K-1])
+
+
