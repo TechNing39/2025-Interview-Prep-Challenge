75375d3f463307035f98da33faae20b738d48953
TechNing39 <leeyujineee@gmail.com>
2025-11-21 18:45:36 +0900

    251121 K번째 수 슬라이싱으로 수정

--- codingtest/K번째 수.py
+++ codingtest/K번째 수.py
@@ -1,13 +1,10 @@
 T=int(input())
 for i in range(T):  
     N,s,e,k=map(int,input().split())
-    num=[]
-    num2=[]
 
     num=list(map(int,input().split()))
 
-    for x in range(s-1,e):
-        num2.append(num[x])
+    num2=num[s-1:e]
     num2.sort()
 
-    print(num2[k-1])
+    print("#%d %d" % (i + 1, num2[k - 1]))
